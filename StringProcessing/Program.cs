c15e193136c46efffdd52e5d3bc391d94a803371
Chris Schmalz <cschmalz03@gmail.com>
2023-01-31 23:31:41 -0500

    initial commit

---
+++ StringProcessing/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+
+bool playagain = true;
+while (playagain)
+{
+    Console.Write("Enter a sentence: ");
+    string sentence = Console.ReadLine();
+
+    foreach (string item in sentence.Split(" "))
+    {
+        Console.WriteLine(item);
+    }
+
+    Console.Write("Would you like to continue (y/n) ?: ");
+    string answer = Console.ReadLine();
+
+    if (answer == "n")
+    {
+        Console.WriteLine("Goodbye!");
+        playagain = false;
+    }
+
+}
+
+
+List<string> words = new List<string>();
+int counter = 1;
+for (int i = 0; i < counter; i++)
+{
+    Console.Write("Enter some text: ");
+    string sentence = Console.ReadLine();
+    words.Add(sentence);
+
+    Console.Write("You have entered: ");
+
+    foreach (string word in words)
+    {
+        Console.Write($"{word} ");
+    }
+
+    Console.WriteLine();
+
+    Console.Write("Would you like to continue (y/n) ?: ");
+    string answer = Console.ReadLine();
+
+    if (answer == "n")
+    {
+        Console.WriteLine("Goodbye!");
+        break;
+    }
+    counter = counter + 1;
+}
